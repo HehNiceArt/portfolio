093abba4aa2bef365e841074fdcb334cff331a90
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-06-07 14:02:43 +0800

    Header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,46 @@ import Images from './components/Images';
 function App() {
   return (
     <div className="App">
-        <Images />
-    </div>
+      <div className="App-header">
+        <div className="Head-l">
+          <p>HehNiceArt</p>
+        <div className="Head-r">
+          <ul className='Menu-top'>
+          <li className='Menu-item'>
+            <div className="Item-label">
+              Illustrations
+            </div>
+          </li>
+          <li className='Menu-item'>
+            <div className="Item-label">
+              Animations
+            </div>
+            </li>
+          <li className='Menu-item'>
+            <div className="Item-label">
+              Live2D Rigs
+            </div>
+            </li>
+          <li className='Menu-item'>
+            <div className="Item-label">
+              Game Projects
+            </div>
+            </li>
+            <li className='Menu-item'>
+              <div className='Item-label'>
+                World
+              </div>
+            </li>
+            <li className='Menu-item'>
+              <div className='Item-label'>
+                Contact
+              </div>
+            </li>
+          </ul>
+          </div>
+        </div>
+        </div>
+      </div>
   );
 }
-
-export default App;
+export default App; 
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Background from '../assets/UI_bg.jpg';
 import './Images.css'
-
 function BackgroundUI(){
   return (
-    <div>
-       <img src={Background} className="Background"/>
+    <div className="Background">
     </div>
   );
 }
