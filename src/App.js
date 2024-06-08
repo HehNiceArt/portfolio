fda29328633d8a6c6b231d20cda2ea03fb30233a
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-06-08 16:25:48 +0800

    Responsive navbar with hamburger

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
 import './App.css';
+import NavigationBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <div className="Head-l">
-          <p>HehNiceArt</p>
-        {/*TODO Choose a font!*/}
-        <div className="Head-r">
-          <ul className='Menu-top'>
-          <li className='Menu-item'>
-            <div className="Item-label">
-              Illustrations
-            </div>
-          </li>
-          <li className='Menu-item'>
-            <div className="Item-label">
-              Animations
-            </div>
-            </li>
-          <li className='Menu-item'>
-            <div className="Item-label">
-              Live2D Rigs
-            </div>
-            </li>
-          <li className='Menu-item'>
-            <div className="Item-label">
-              Game Projects
-            </div>
-            </li>
-            <li className='Menu-item'>
-              <div className='Item-label'>
-                World
-              </div>
-            </li>
-            <li className='Menu-item'>
-              <div className='Item-label'>
-              Contact
-              </div>
-            </li>
-          </ul>
-          </div>
-        </div>
-        </div>
-      </div>
+      <NavigationBar />
+    </div>
   );
 }
 export default App; 
