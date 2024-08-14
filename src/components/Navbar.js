3034a753fa7097390f3b9e3233be6333693dbd51
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-08-15 00:22:26 +0800

    polish ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,51 +25,37 @@ function NavigationBar() {
     <div>
       <nav>
         <ul className={`Sidebar ${isSidebarVisible ? 'active' : ''}`}>
-          <li onClick={toggleSidebar}>
+          <li className="cross" onClick={toggleSidebar}>
             <a href='#'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                height='24px'
-                viewBox='0 -960 960 960'
-                width='24px'
-                fill='#000000'
-              >
+              <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#000000'>
                 <path d='m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z' />
               </svg>
             </a>
           </li>
-          <li><a href='#'>Who is Nice?</a></li>
-          <li><a href='#'>Illustrations</a></li>
-          <li><a href='#'>Animations</a></li>
-          <li><a href='#'>Live2D Rigs</a></li>
-          <li><a href='#'>Game Projects</a></li>
-          <li><a href='#'>World</a></li>
-          <li><a href='#'>Contact</a></li>
+          <li className='sidebar-links'><a href='#'>Who is Nice?</a></li>
+          <li className='sidebar-links'><a href='#'>Illustrations</a></li>
+          <li className='sidebar-links'><a href='#'>Animations</a></li>
+          <li className='sidebar-links'><a href='#'>Live2D Rigs</a></li>
+          <li className='sidebar-links'><a href='#'>Game Projects</a></li>
+          <li className='sidebar-links'><a href='#'>World</a></li>
+          <li className='sidebar-links'><a href='#'>Contact</a></li>
         </ul>
       </nav>
       <nav className='App-header'>
         <ul>
-          <li>
-            <a>
-              <p><strong>HehNiceArt</strong></p>
-            </a>
+          <li className='title-cover'>
+            <a className='title'> <p><strong>HehNiceArt</strong></p></a>
           </li>
-          <li className='hideOnMobile'><a href='#'>Who is Nice?</a></li>
-          <li className='hideOnMobile'><a href='#'>Illustrations</a></li>
-          <li className='hideOnMobile'><a href='#'>Animations</a></li>
-          <li className='hideOnMobile'><a href='#'>Live2D Rigs</a></li>
-          <li className='hideOnMobile'><a href='#'>Game Projects</a></li>
-          <li className='hideOnMobile'><a href='#'>World</a></li>
-          <li className='hideOnMobile'><a href='#'>Contact</a></li>
+          <li className='hideOnMobile'><a className="nav-links" href='#'>Who is Nice?</a></li>
+          <li className='hideOnMobile'><a className="nav-links" href='#'>Illustrations</a></li>
+          <li className='hideOnMobile'><a className="nav-links" href='#'>Animations</a></li>
+          <li className='hideOnMobile'><a className="nav-links" href='#'>Live2D Rigs</a></li>
+          <li className='hideOnMobile'><a className="nav-links" href='#'>Game Projects</a></li>
+          <li className='hideOnMobile'><a className="nav-links" href='#'>World</a></li>
+          <li className='hideOnMobile'><a className="nav-links" href='#'>Contact</a></li>
           <li onClick={toggleSidebar} className='Hamburger'>
             <a href='#'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                height='22px'
-                viewBox='0 -960 960 960'
-                width='22px'
-                fill='#000000'
-              >
+              <svg xmlns='http://www.w3.org/2000/svg' height='22px' viewBox='0 -960 960 960' width='22px' fill='#000000'>
                 <path d='M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z' />
               </svg>
             </a>
