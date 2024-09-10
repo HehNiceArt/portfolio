ae82d014e9a3d1040bd12c0d5ff94f3cbcebcf9e
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-09-10 10:54:34 +0800

    change cursorrules

--- src/components/Choices.js
+++ src/components/Choices.js
@@ -1,19 +1,26 @@
 import React from "react";
 import './HeadText.css'
 
-const Choices = ({
-    first,
-    second
-}) => {
+const Choices = ({ first, second, third }) => {
+    const shouldRenderFirst = !(second && third);
+
     return (
         <div className="Head-Container">
             <div className="Head-TextContainer">
-                <p className="Head-Text">{first}</p>
-                <div style={{ width: '20px' }}></div>
-                <p className="Head-TextNot">{second}</p>
+                {shouldRenderFirst && (
+                    <p className="Head-Text" tabIndex="0" aria-label={first} onClick={() => console.log(first)}>
+                        {first}
+                    </p>
+                )}
+                <p className="Head-TextNot" tabIndex="0" aria-label={second} onClick={() => console.log(second)}>
+                    {second}
+                </p>
+                <p className="Head-TextThird" tabIndex="0" aria-label={third} onClick={() => console.log(third)}>
+                    {third}
+                </p>
             </div>
         </div>
-    )
+    );
 }
 
 export default Choices;
